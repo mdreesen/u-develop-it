81ba747b92500eddd4dc381cd006ac78221802f6
Michael Dreesen <michaeldreesen90@gmail.com>
2020-10-05 19:14:44 -0600

    added insert sql into seeds.sql file

--- db/seeds.sql
+++ db/seeds.sql
@@ -5,8 +5,8 @@ VALUES
 ('Piers', 'Gaveston', 0),
 ('Charles', 'LeRoi', 1),
 ('Katherine', 'Mansfield', 1),
-  ('Dora', 'Carrington', 0),
-  ('Edward', 'Bellamy', 0),
-  ('Montague', 'Summers', 1),
-  ('Octavia', 'Butler', 1),
-  ('Unica', 'Zurn', 1);
+('Dora', 'Carrington', 0),
+('Edward', 'Bellamy', 0),
+('Montague', 'Summers', 1),
+('Octavia', 'Butler', 1),
+('Unica', 'Zurn', 1);
